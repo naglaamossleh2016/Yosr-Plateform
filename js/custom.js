f7c1ee7f015d1742322afd7de71d518911b8dbb3
naglaamossleh2016 <naglaa.mossleh@gmail.com>
2025-05-01 09:13:41 +0200

    change mobil menu bg

--- js/custom.js
+++ js/custom.js
@@ -16,78 +16,18 @@
     // Initialize WOW.js for animations
     new WOW().init();
 
-    // متغيرات القائمة
-    const navbarToggler = $('.navbar-toggler');
-    const togglerIcon = navbarToggler.find('i');
-    const mobileMenu = $('.mobile-menu');
-    const closeMenu = $('.close-menu');
-    const menuBackdrop = $('.mobile-menu-backdrop');
-    const body = $('body');
-      
-    // الأيقونات
-    const menuIcon = 'fa-bars'; // أيقونة البرجر
-    const closeIcon = 'fa-times'; // أيقونة الإغلاق
-    
-    // فتح القائمة
-    navbarToggler.on('click', function(e) {
+    // Fix navbar toggler to prevent redirect and ensure menu opens
+    $('.navbar-toggler').on('click', function (event) {
+      event.preventDefault(); // Prevent default behavior
+      event.stopPropagation(); // Stop event propagation to parent elements
+      $('#main-navbar').collapse('toggle'); // Manually toggle the menu
+    });
 
-      e.preventDefault();
-      mobileMenu.addClass('show');
-      menuBackdrop.addClass('show');
-      body.addClass('menu-open');
-      
-      // تغيير الأيقونة إلى إغلاق
-      togglerIcon.removeClass(menuIcon).addClass(closeIcon);
-    });
-    
-    // إغلاق القائمة
-    function closeMobileMenu() {
-      mobileMenu.removeClass('show');
-      menuBackdrop.removeClass('show');
-      body.removeClass('menu-open');
-      
-      // تغيير الأيقونة إلى برجر
-      togglerIcon.removeClass(closeIcon).addClass(menuIcon);
+    // Optional: Disable slicknav if it conflicts with Bootstrap
+    if ($.fn.slicknav) {
+      $('.navbar-nav').slicknav('destroy'); // Destroy slicknav if causing issues
     }
-    
-    // إغلاق بالزر
-    closeMenu.on('click', function(e) {
-      
-      e.preventDefault();
-      closeMobileMenu();
-    });
-    
-    // إغلاق بالتعتيم
-    menuBackdrop.on('click', function(e) {
-      e.preventDefault();
-      closeMobileMenu();
-    });
-    
-    // إغلاق عند النقر على رابط
-    $('.mobile-menu li a').on('click', function(e) {
-      if ($(this).attr('href').startsWith('#')) {
-        e.preventDefault();
-        const target = $($(this).attr('href'));
-        if (target.length) {
-          closeMobileMenu();
-          setTimeout(() => {
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 800);
-          }, 300);
-        }
-      } else {
-        closeMobileMenu();
-      }
-    });
-    
-    // إخفاء القائمة عند التمرير
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-        $('.navbar').addClass('navbar-scrolled');
-      } else {
-        $('.navbar').removeClass('navbar-scrolled');
-      }
-    });
   });
 })(jQuery);
+// عند تحميل الصفحة
+// تنشيط القائمة الطافية
